1d02a07c856f94dacb14ec1e4f944e6d92b684c8
bpetryshyn <b.y.petryshyn@gmail.com>
2021-05-15 14:04:43 +0300

    Split Client class to ClientCore and ClientPopulated

--- src/users/entities/client.ts
+++ src/users/entities/client.ts
@@ -1,27 +1,78 @@
 import { Exclude, Expose, Type } from 'class-transformer';
 import { Prop, Schema } from '@nestjs/mongoose';
 import { User } from './user';
-import { ValidateNested } from 'class-validator';
-import * as mongoose from 'mongoose';
+import { IsMongoId, ValidateNested } from 'class-validator';
 
-export interface ClientConstructorParams {
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ClientInfoConstructorParams {}
+
+interface ClientCoreConstructorParams extends ClientInfoConstructorParams {
+  user: string;
+}
+
+interface ClientConstructorParams {
+  user: string;
+  id: string;
+}
+
+interface ClientPopulatedConstructorParams {
+  id: string;
   user: User;
 }
 
 @Exclude()
 @Schema()
-export class Client {
+export class ClientInfo {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor({}: ClientInfoConstructorParams) {}
+}
+
+@Exclude()
+@Schema()
+export class ClientCore extends ClientInfo {
   @Expose()
-  @Type(() => User)
-  @ValidateNested()
+  @IsMongoId()
   @Prop({
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     ref: User.name,
     required: true,
   })
+  user: string;
+
+  constructor({ user }: ClientCoreConstructorParams) {
+    super({});
+    this.user = user;
+  }
+}
+
+@Exclude()
+@Schema()
+export class Client extends ClientCore {
+  @Expose()
+  @IsMongoId()
+  id: string;
+
+  constructor({ user, id }: ClientConstructorParams) {
+    super({ user });
+    this.id = id;
+  }
+}
+
+@Exclude()
+@Schema()
+export class ClientPopulated extends ClientInfo {
+  @Expose()
+  @IsMongoId()
+  id: string;
+
+  @Expose()
+  @Type(() => User)
+  @ValidateNested()
   user: User;
 
-  constructor({ user }: ClientConstructorParams) {
+  constructor({ id, user }: ClientPopulatedConstructorParams) {
+    super({});
+    this.id = id;
     this.user = user;
   }
 }
