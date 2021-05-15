186e1765ad0984d266dc9181eec3720a75e48f34
bpetryshyn <b.y.petryshyn@gmail.com>
2021-05-15 14:07:15 +0300

    Revert "Split Client class to ClientCore and ClientPopulated"
    This reverts commit 1d02a07c

--- src/users/entities/client.ts
+++ src/users/entities/client.ts
@@ -1,78 +1,27 @@
 import { Exclude, Expose, Type } from 'class-transformer';
 import { Prop, Schema } from '@nestjs/mongoose';
 import { User } from './user';
-import { IsMongoId, ValidateNested } from 'class-validator';
+import { ValidateNested } from 'class-validator';
+import * as mongoose from 'mongoose';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface ClientInfoConstructorParams {}
-
-interface ClientCoreConstructorParams extends ClientInfoConstructorParams {
-  user: string;
-}
-
-interface ClientConstructorParams {
-  user: string;
-  id: string;
-}
-
-interface ClientPopulatedConstructorParams {
-  id: string;
+export interface ClientConstructorParams {
   user: User;
 }
 
 @Exclude()
 @Schema()
-export class ClientInfo {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor({}: ClientInfoConstructorParams) {}
-}
-
-@Exclude()
-@Schema()
-export class ClientCore extends ClientInfo {
-  @Expose()
-  @IsMongoId()
-  @Prop({
-    type: String,
-    ref: User.name,
-    required: true,
-  })
-  user: string;
-
-  constructor({ user }: ClientCoreConstructorParams) {
-    super({});
-    this.user = user;
-  }
-}
-
-@Exclude()
-@Schema()
-export class Client extends ClientCore {
-  @Expose()
-  @IsMongoId()
-  id: string;
-
-  constructor({ user, id }: ClientConstructorParams) {
-    super({ user });
-    this.id = id;
-  }
-}
-
-@Exclude()
-@Schema()
-export class ClientPopulated extends ClientInfo {
-  @Expose()
-  @IsMongoId()
-  id: string;
-
+export class Client {
   @Expose()
   @Type(() => User)
   @ValidateNested()
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: User.name,
+    required: true,
+  })
   user: User;
 
-  constructor({ id, user }: ClientPopulatedConstructorParams) {
-    super({});
-    this.id = id;
+  constructor({ user }: ClientConstructorParams) {
     this.user = user;
   }
 }
