fcc59877ee40f8145483203f8f24a773bd488d3a
bpetryshyn <b.y.petryshyn@gmail.com>
2021-05-25 22:46:50 +0300

    Expose appointments via HTTP service

--- src/appointments/appointments.controller.ts
+++ src/appointments/appointments.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import { AppointmentsService } from './appointments.service';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+import { AppointmentCreateRequest } from './entities/appointment-create-request';
+import { Appointment } from './entities/appointment';
+import { AuthedRequest } from '../auth/jwt/authed-request';
 
 @Controller('appointments')
-export class AppointmentsController {}
+export class AppointmentsController {
+  constructor(private readonly appointmentsService: AppointmentsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async create(
+    @Req() request: AuthedRequest,
+    @Body() createRequest: AppointmentCreateRequest,
+  ): Promise<Appointment> {
+    const userEmail = request.user.email;
+    const masterEmail = createRequest.masterEmail;
+    if (masterEmail != userEmail) {
+      throw new UnauthorizedException(
+        `User ${userEmail} can't create appointments for master ${masterEmail}`,
+      );
+    }
+    return this.appointmentsService.create(createRequest);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('master')
+  async getAllMasterAppointments(
+    @Req() request: AuthedRequest,
+  ): Promise<Appointment[]> {
+    return this.appointmentsService.getByMasterEmail(request.user.email);
+  }
+}
